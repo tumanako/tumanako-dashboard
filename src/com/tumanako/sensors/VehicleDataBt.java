26b86df0a9ea4d8840dea817787a183c230032f0
Jeremy Cole-Baker <jeremy@rhtech.co.nz>
2012-08-07 22:41:30 +0000

    Small UI update; Added headers to source code
    git-svn-id: tumanako.svn.sourceforge.net::svn/tumanako/trunk@470 c3429617-7ab9-49b5-8233-5f90b2ce515f

--- src/com/tumanako/sensors/VehicleDataBt.java
+++ src/com/tumanako/sensors/VehicleDataBt.java
@@ -1,12 +1,30 @@
 package com.tumanako.sensors;
 
+/************************************************************************************
+Tumanako - Electric Vehicle and Motor control software
+
+Copyright (C) 2012 Jeremy Cole-Baker <jeremy@rhtech.co.nz>
+
+This file is part of Tumanako Dashboard.
+
+Tumanako is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Tumanako is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Tumanako.  If not, see <http://www.gnu.org/licenses/>.
+
+*************************************************************************************/
+
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
-
-import com.tumanako.dash.DashData;
-
-
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
